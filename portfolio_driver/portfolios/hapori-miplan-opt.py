f378587f0e5062c5f42b420891d4a12c4da0fc97
Alberto Pozanco <apozanco@pa.uc3m.es>
2023-03-23 09:17:40 +0000

    Add portfolio files and Apptainer images for MIPlan portfolios

---
+++ portfolio_driver/portfolios/hapori-miplan-opt.py
@@ -0,0 +1,11 @@
+PLANNERS = [
+    (861, ['ipc2018-opt-scorpion', 'default']),
+    (321, ['ipc2014-opt-symba1', 'default']),
+    (263, ['ipc2018-opt-metis', 'metis2']),
+    (134, ['ipc2018-decstar', 'opt-config06']),
+    (128, ['ipc2018-decstar', 'opt-config00']),
+    (55, ['ipc2018-opt-delfi', 'h2-simpless-oss-cpdbshc900']),
+    (25, ['ipc2018-opt-delfi', 'simpless-oss-masb50kmiasmdfp']),
+    (7, ['ipc2018-decstar', 'opt-config01']),
+    (3, ['ipc2018-opt-complementary2', 'default']),
+]
